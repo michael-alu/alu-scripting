65b7cb86e6fd0f2f878033922683262fde73964c
Michael <m.nwuju@alustudent.com>
2025-02-21 13:56:47 -0800

    task 1-top

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-""" top_ten.py - Queries Reddit API for the top 10 hot posts in a subreddit """
+""" top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed in a subreddit."""
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
         return
-    
-    data = response.json().get('data', {})
-    posts = data.get('children', [])
-    
-    if not posts:
-        print(None)
-        return
-    
-    print("OK")  # This line is to satisfy the "OK" expectation in the output check
+    posts = response.json()['data']['children']
     for post in posts:
         print(post['data']['title'])
-
